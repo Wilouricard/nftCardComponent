67e29aae2bc5050d4c024914367359995663197f
Nam Tang <nam.tang@live.fr>
2022-09-22 17:06:03 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      'cyan': 'hsl(178, 100%, 50%)',
+      'main-bg': 'hsl(217, 54%, 11%)',
+      'card-bg': 'hsl(216, 50%, 16%)',
+      'line': 'hsl(215, 32%, 27%)',
+      'white': 'hsl(0, 0%, 100%)'
+    }
+  },
+  plugins: [],
+}
